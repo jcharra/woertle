338b73bcc838bea25fb22f5751dde739aa664264
Johannes Charra <johannes.charra@gmail.com>
2022-01-09 15:01:47 +0100

    add react and tailwind css

---
+++ src/App.js
@@ -0,0 +1,47 @@
+function App() {
+  return (
+    <div className="App">
+      <div class="p-20 grid grid-cols-5 w-1/2 place-content-center">
+        <div class="rounded-tl-3xl border border-solid p-2">01</div>
+        <div class="border border-l-0 border-solid p-2">01</div>
+        <div class="border border-l-0 border-solid p-2">01</div>
+        <div class="border border-l-0 border-solid p-2">01</div>
+        <div class="rounded-tr-3xl border border-l-0 border-solid p-2">01</div>
+
+        <div class="border border-t-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+
+        <div class="border border-t-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+
+        <div class="border border-t-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+
+        <div class="border border-t-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+
+        <div class="rounded-bl-3xl border-t-0 border border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="border border-t-0 border-l-0 border-solid p-2">01</div>
+        <div class="rounded-br-3xl border-t-0 border border-l-0 border-solid p-2">
+          01
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

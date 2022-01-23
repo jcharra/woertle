0b742206b9c5bc1e2c358c1a72535a363c8bbc1c
Johannes Charra <johannes.charra@gmail.com>
2022-01-23 17:13:20 +0100

    forgot depcy

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -115,7 +115,7 @@ export function GameContextProvider({ children }: { children: React.ReactNode })
       setSolved(false);
       setCaption("");
     },
-    [setGuesses, setTargetWord, setCursorRow, setSolved, setCaption]
+    [setGuesses, setTargetWord, setCursorRow, setSolved, setCaption, kidsMode]
   );
 
   const toggleKidsMode = useCallback(() => {
